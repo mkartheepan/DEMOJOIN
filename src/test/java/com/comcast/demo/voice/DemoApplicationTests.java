138cdbbfab3aebbd0288f55d7bc7bba111733199
Kartheepan Manickam <Kartheepan_Manickam@comcast.com>
2022-11-28 21:55:41 +0530

    Initial commit

---
+++ src/test/java/com/comcast/demo/voice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comcast.demo.voice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
